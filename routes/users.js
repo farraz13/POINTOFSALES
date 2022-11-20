3d3ca70ac6a63f649df0e6272c3f3af2433fb1b9
farraz13 <farraz.f13@gmail.com>
2022-11-20 22:02:46 +0700

    not done yet

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+
+module.exports = (db) =>{
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-module.exports = router;
+router.get('/datatable', async (req, res) => {
+  let params = []
+
+  if(req.query.search.value){
+      params.push(`username ilike '%${req.query.search.value}%'`)
+  }
+
+  const limit = req.query.length
+  const offset = req.query.start
+  const sortBy = req.query.columns[req.query.order[0].column].data
+  const sortMode = req.query.order[0].dir
+
+  const total = await db.query(`select count(*) as total from users${params.length > 0 ? ` where ${params.join(' or ')}` : ''}`)
+  const data = await db.query(`select * from users${params.length > 0 ? ` where ${params.join(' or ')}` : ''} order by ${sortBy} ${sortMode} limit ${limit} offset ${offset} `)
+  const response = {
+      "draw": Number(req.query.draw),
+      "recordsTotal": total.rows[0].total,
+      "recordsFiltered": total.rows[0].total,
+      "data": data.rows
+    }
+  res.json(response)
+})
+
+return router;
+};
