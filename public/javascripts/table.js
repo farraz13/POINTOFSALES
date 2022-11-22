6e194cce3d233040764a2b6aff7d8403b9927b55
farraz13 <farraz.f13@gmail.com>
2022-11-22 14:45:44 +0700

    not done yet

--- public/javascripts/table.js
+++ public/javascripts/table.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $('#tableUsers').DataTable({
-        "lengthMenu": [[3, 5,10, -1], [3, 5, 10, "All"]],
+        "lengthMenu": [[3, 5, 10, -1], [3, 5, 10, "All"]],
         "processing": true,
         "serverSide": true,
         "ajax": "/users/datatable",
@@ -11,11 +11,35 @@ $(document).ready(function () {
             { "data": "role" },
             {
                 "data": "userid",
-                render : function (data){
+                render: function (data) {
                     return `
                     
                     <a type="button" href ="/users/edit/${data}" class='btn btn-success rounded-circle'><i class="fa-solid fa-circle-info"></i></a>
-                    <a type="button" href ="/users/delete/${data}" class='btn btn-danger rounded-circle'><i class="fa-solid fa-trash"></i></a>
+                   
+<button type="button" class="btn btn-danger rounded-circle" data-toggle="modal" data-target="#exampleModal${data}">
+<i class="fa-solid fa-trash"></i>
+</button>
+
+
+<div class="modal fade" id="exampleModal${data}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        Are you sure want to delete it?
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <a href="/users/delete/${data}" type="button" class="btn btn-primary">Save changes</a>
+      </div>
+    </div>
+  </div>
+</div>
                     
                     `
                 }
@@ -23,3 +47,4 @@ $(document).ready(function () {
         ]
     });
 });
+
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+const bcrypt = require('bcrypt')
+const saltRounds = 10;
 
 module.exports = (db) =>{
 router.get('/', function(req, res, next) {
@@ -46,7 +48,7 @@ router.get('/delete/:userid', function (req, res, next) {
 
 //EDIT
 router.get('/edit/:userid', function (req, res, next) {
-  db.query('SELECT * FROM users WHERE userid =$1', [Number(req.params.userid)], (err, data) => {
+  db.query('SELECT * FROM users WHERE userid =$1', [req.params.userid], (err, data) => {
     if (err) return res.send(err, 'error bang')
     if (data.rows.length == 0) return res.send(err, 'data not found')
     res.render('usersPage/edit', { item: data.rows[0] })
@@ -64,6 +66,32 @@ router.post('/edit/:userid', function (req, res, next) {
   })
 })
 //akhirEDIT
+
+//ADD
+router.get('/add', function (req, res, next) {
+    res.render('usersPage/add')
+
+});
+
+router.post('/add', async function (req, res, next) {
+  try {
+    console.log(req.body)
+    const { name, email, password, role } = req.body
+
+    const { rows: emails } = await db.query('SELECT * FROM users WHERE email = $1', [email])
+
+    if (emails.length > 0) return res.send('email sudah ada')
+
+    const hash = bcrypt.hashSync(password, saltRounds);
+    await db.query('INSERT INTO users (email, name, password, role) VALUES ($1, $2, $3, $4)', [email, name, hash, role])
+
+    res.redirect('/users')
+  } catch (e) {
+    console.log(e)
+    res.redirect('/users')
+  }
+});
+//akhirADD
 
 
 return router;
